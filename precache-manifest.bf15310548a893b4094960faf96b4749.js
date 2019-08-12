c86349883061e7750335823d138f6e981f13df62
babyazalea <usun12@hotmail.com>
2019-08-12 11:12:59 +0900

    Updates

---
+++ precache-manifest.bf15310548a893b4094960faf96b4749.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e19aff214a17f93e4d5ab0bfff728c8",
+    "url": "/react_todolist/index.html"
+  },
+  {
+    "revision": "74be0136b2d7d0828016",
+    "url": "/react_todolist/static/js/2.2ab8066a.chunk.js"
+  },
+  {
+    "revision": "a90cf43c10025fbc7307",
+    "url": "/react_todolist/static/js/main.48e876b2.chunk.js"
+  },
+  {
+    "revision": "bde690fe8b42ce56f115",
+    "url": "/react_todolist/static/js/runtime~main.60686b0e.js"
+  }
+]);
